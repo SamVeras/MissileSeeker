a341a2a5895bccda4be7a94fa06a2d8242d9e761
Samuel Veras <samuel.bernardo.veras@gmail.com>
2023-11-17 17:33:52 -0300

    Wrapper para sdl_draw_point com vetor

--- include/SDL_wrappers.h
+++ include/SDL_wrappers.h
@@ -3,3 +3,4 @@
 #include "vetor.h"
 
 int draw_line(SDL_Renderer* renderer, Vetor A, Vetor B);
+int draw_point(SDL_Renderer* renderer, Vetor P);
--- src/SDL_wrappers.cpp
+++ src/SDL_wrappers.cpp
@@ -2,4 +2,8 @@
 
 int draw_line(SDL_Renderer* renderer, Vetor A, Vetor B) {
    return SDL_RenderDrawLine(renderer, A.x, A.y, B.x, B.y);
-}
+};
+
+int draw_point(SDL_Renderer* renderer, Vetor P) {
+   return SDL_RenderDrawPoint(renderer, P.x, P.y);
+};
