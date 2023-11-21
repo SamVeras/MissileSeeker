45ad086fceca31a0cb280878c0d712c189f47376
Samuel Veras <samuel.bernardo.veras@gmail.com>
2023-11-21 13:46:31 -0300

    Global defined namespace

---
+++ include/defined.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "vetor.h"
+
+namespace DEFINED {
+const int WIN_W = 500;
+const int WIN_H = 900;
+const Vetor WIN_CENTER = {WIN_W / 2, WIN_H / 2};
+const double GRAVITY = -0.1;
+}  // namespace DEFINED
--- include/global.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "vetor.h"
-namespace global {
-const int WINDOW_W = 700;
-const int WINDOW_H = 700;
-const Vetor true_center = {WINDOW_W / 2, WINDOW_H / 2};
-}
